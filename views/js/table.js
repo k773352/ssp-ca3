728463addd5384a2e012adfd39f06169d1e31819
Ruby <k773352@gmail.com>
2022-01-06 01:00:08 +0000

    add code

--- views/js/table.js
+++ views/js/table.js
@@ -1,18 +1,55 @@
-function draw_table(){
+function draw_table()
+{
     $("#results").empty();
-    $.getHTMLuncached = function(url) {
-        return $.ajax({
+    $.getHTMLuncached = function(url) 
+    {
+        return $.ajax(
+        {
             url: url,
             type: 'GET',
             cache: false,
-            success: function(html){
+            success: function(html)
+            {
                 $("#results").append(html);
+                select_row();
             }
         });
     };
     $.getHTMLuncached("/get/html");
 };
 
-$(document).ready(function(){
+function select_row()
+ {
+ 	$("#menuTable tbody tr[id]").click(function ()
+ 	{
+ 		$(".selected").removeClass("selected");
+ 		$(this).addClass("selected");
+ 		var section = $(this).prevAll("tr").children("td[colspan='3']").length - 1;
+ 		var entree = $(this).attr("id") - 1;
+ 		delete_row(section, entree);
+ 	})
+};
+
+function delete_row(sec, ent)
+{
+	$("#delete").click(function ()
+	{
+		$.ajax(
+		{
+			url: "/post/delete",
+			type: "POST",
+			data:
+			{
+				section: sec,
+				entree: ent
+			},
+			cache: false,
+			success: setTimeout(draw_table, 1000)
+		})
+	})
+};
+     
+    $(document).ready(function()
+    {
     draw_table();
 }) 
